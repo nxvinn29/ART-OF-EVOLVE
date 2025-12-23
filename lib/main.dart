1ce1a4083da916881350bde81dcce2c26ddcecda
nxvinn29 <169894816+nxvinn29@users.noreply.github.com>
2025-12-23 09:27:19 +0530

    refactor: sort imports in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,12 @@
 // initializes Hive storage, and runs the root [ArtOfEvolveApp].
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'src/app.dart';
-import 'src/services/storage/hive_service.dart';
-
 import 'package:firebase_core/firebase_core.dart';
+
 import 'firebase_options.dart';
+import 'src/app.dart';
 import 'src/services/notifications/notification_service.dart';
+import 'src/services/storage/hive_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
